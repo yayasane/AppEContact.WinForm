dd8c1b2137d303ab8e02a4280444d641bf2ba2ad
yayaveli <yabadji2010@gmail.com>
2020-12-01 23:58:44 +0000

    Initial

---
+++ AppEContact.WinForm/Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEContact.WinForm.Models
+{
+    class Contact
+    {
+        public int id { get; set; }
+        public string NomComplet { get; set; }
+        public DateTime DateNaiss { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Genre { get; set; }
+        public string UrlPhoto { get; set; }
+
+        public Contact(string nomComplet, DateTime dateNaiss, string email, string tel, string genre, string urlPhoto)
+        {
+            NomComplet = nomComplet;
+            DateNaiss = dateNaiss;
+            Email = email;
+            Tel = tel;
+            Genre = genre;
+            UrlPhoto = urlPhoto;
+        }
+
+        public Contact()
+        {
+        }
+    }
+}
